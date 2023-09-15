4b2aa7bf2724c32d134f41728398b619b2b191f2
KimSeoYeon23 <sollucidus0221@naver.com>
2023-09-16 01:12:34 +0900

    Add Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,65 @@
+<script>
+
+</script>
+
+<template>
+  <footer>
+    <div>
+      <a href="https://github.com/codestates-beb/beb-09-clean-mile" target="_blank">
+        <font-awesome-icon :icon="['fab', 'github']" size="2xl"/>
+      </a>
+      <a href="javascript:void(0);">
+        <font-awesome-icon :icon="['fab', 'instagram']" size="2xl"/>
+      </a>
+      <a href="javascript:void(0);">
+        <font-awesome-icon icon="envelope" size="2xl"/>
+      </a>
+      <a href="javascript:void(0);">
+        <font-awesome-icon :icon="['fab', 'discord']" size="2xl"/>
+      </a>
+    </div>
+    <div>
+      <p>Copyright &copy; 2023 Clean Mile</p>
+    </div>
+  </footer>
+
+</template>
+
+<style lang="scss">
+@import '../assets/variables.scss';
+
+footer {
+  width: 100%;
+  height: 14rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 3rem;
+  background-color: rgb(21 128 61);
+
+  div {
+    &:first-child {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 3rem;
+
+      a {
+        color: white;
+        border: 1px solid white;
+        border-radius: 0.75rem;
+        padding: 0.75rem;
+        cursor: pointer;
+      }
+    }
+
+    &:last-child {
+      p {
+        color: white;
+        font-weight: 500;
+      }
+    }
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-import './assets/main.css'
+import './assets/main.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faDiscord, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+library.add(faGithub, faDiscord, faInstagram, faEnvelope);
+
+const app = createApp(App);
+
+app.component('font-awesome-icon', FontAwesomeIcon);
+
+app.mount('#app');
